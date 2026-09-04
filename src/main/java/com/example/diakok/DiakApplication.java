fdde238a822bf11fd8bca38016277b26dfc96cc6
Weicat <weicat@mailfence.com>
2026-09-04 12:05:58 +0200

    Layout basics

--- src/main/java/com/example/diakok/DiakApplication.java
+++ src/main/java/com/example/diakok/DiakApplication.java
@@ -11,7 +11,7 @@ public class DiakApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(DiakApplication.class.getResource("diak-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 960, 540);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/diakok/DiakController.java
+++ src/main/java/com/example/diakok/DiakController.java
@@ -1,14 +1,50 @@
 package com.example.diakok;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class DiakController {
     @FXML
-    private Label welcomeText;
+    private Button saveButton;
+    @FXML
+    private Button everyoneButton;
+    @FXML
+    private Button sandorButton;
+    @FXML
+    private Button kecskemetButton;
+    @FXML
+    private Button from1996Button;
+    @FXML
+    private Button partOf10AButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onSaveButtonClick() {
+
+    }
+
+    @FXML
+    protected void onEveryoneButtonClick() {
+
+    }
+
+    @FXML
+    protected void onSandorButtonClick() {
+
+    }
+
+    @FXML
+    protected void onKecskemetButtonClick() {
+
+    }
+
+    @FXML
+    protected void onFrom1996Click() {
+
+    }
+
+    @FXML
+    protected void onPartOf10AButtonClick() {
+
     }
 }
