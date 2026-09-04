d7c0cf8aafb2032183418262ab896f5d90bbead6
Weicat <weicat@mailfence.com>
2026-09-04 17:35:44 +0200

    File saving works

--- src/main/java/com/example/diakok/DiakController.java
+++ src/main/java/com/example/diakok/DiakController.java
@@ -5,25 +5,45 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
+import java.io.FileWriter;
+
 interface StudentFilter {
     boolean is_matched(Student student);
 }
 
 public class DiakController {
     private final Students students = new Students(Students.loadFromFile("/com/example/diakok/diakok.csv"));
+    private String[] current_content = new String[0];
 
     @FXML
     private ListView<String> people_list;
 
     @FXML
     protected void onSaveButtonClick() {
+        try {
+/* print cwd
+            Path currentRelativePath = Paths.get("");
+            String s = currentRelativePath.toAbsolutePath().toString();
+            System.out.println("Current absolute path is: " + s);
+*/
+            FileWriter writer = new FileWriter("adatok.txt");
+            writer.write("");
 
+            for (String line : current_content) {
+                writer.append(line);
+                writer.append("\n");
+            }
+
+            writer.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     protected void onEveryoneButtonClick() {
         ObservableList<String> content = FXCollections.observableArrayList(
-                filter_people_into_array((Student _) -> true)
+                query_students_by_filter((Student _) -> true)
         );
         people_list.setItems(content);
     }
@@ -31,7 +51,7 @@ public class DiakController {
     @FXML
     protected void onSandorButtonClick() {
         ObservableList<String> content = FXCollections.observableArrayList(
-                filter_people_into_array((Student student) -> student.getKnev().equals("Sándor"))
+                query_students_by_filter((Student student) -> student.getKnev().equals("Sándor"))
         );
         people_list.setItems(content);
     }
@@ -39,7 +59,7 @@ public class DiakController {
     @FXML
     protected void onKecskemetButtonClick() {
         ObservableList<String> content = FXCollections.observableArrayList(
-                filter_people_into_array((Student student) -> student.getPlaceOfLiving().equals("Kecskemét"))
+                query_students_by_filter((Student student) -> student.getPlaceOfLiving().equals("Kecskemét"))
         );
         people_list.setItems(content);
     }
@@ -47,7 +67,7 @@ public class DiakController {
     @FXML
     protected void onFrom1996Click() {
         ObservableList<String> content = FXCollections.observableArrayList(
-                filter_people_into_array((Student student) -> student.getBirthdate().startsWith("1996"))
+                query_students_by_filter((Student student) -> student.getBirthdate().startsWith("1996"))
         );
         people_list.setItems(content);
     }
@@ -55,17 +75,15 @@ public class DiakController {
     @FXML
     protected void onPartOf10AButtonClick() {
         ObservableList<String> content = FXCollections.observableArrayList(
-                filter_people_into_array((Student student) -> student.getSchoolclass().equals("10/A"))
+                query_students_by_filter((Student student) -> student.getSchoolclass().equals("10/A"))
         );
         people_list.setItems(content);
     }
 
-    private String[] filter_people_into_array(StudentFilter filter) {
-        String[] result = new String[] {};
+    private String[] query_students_by_filter(StudentFilter filter) {
         Student[] matches = new Student[] {};
 
         for (Student student : students.getStudents()) {
-            System.out.println(student.getKnev());
             if (filter.is_matched(student)) {
                 matches = Students.append_student_to_array(student, matches);
             }
@@ -73,12 +91,12 @@ public class DiakController {
 
         // Lásd: Students.java => append_student_to_array() fölötti komment
         for (Student student : matches) {
-            String[] extended = new String[result.length + 1];
-            System.arraycopy(result, 0, extended, 0, result.length);
+            String[] extended = new String[current_content.length + 1];
+            System.arraycopy(current_content, 0, extended, 0, current_content.length);
             extended[extended.length - 1] = student.toString();
-            result = extended;
+            current_content = extended;
         }
 
-        return result;
+        return current_content;
     }
 }
