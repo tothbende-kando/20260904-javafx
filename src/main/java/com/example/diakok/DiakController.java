ad32bf9f1239ab54c5897d4384718a57613b65b3
Weicat <weicat@mailfence.com>
2026-09-04 17:41:30 +0200

    The match list is now emptied when the filter is changed instead of growing infinitely

--- src/main/java/com/example/diakok/DiakController.java
+++ src/main/java/com/example/diakok/DiakController.java
@@ -42,46 +42,37 @@ public class DiakController {
 
     @FXML
     protected void onEveryoneButtonClick() {
-        ObservableList<String> content = FXCollections.observableArrayList(
-                query_students_by_filter((Student _) -> true)
-        );
-        people_list.setItems(content);
+        current_content = query_students_by_filter((Student _) -> true);
+        people_list.setItems(FXCollections.observableArrayList(current_content));
     }
 
     @FXML
     protected void onSandorButtonClick() {
-        ObservableList<String> content = FXCollections.observableArrayList(
-                query_students_by_filter((Student student) -> student.getKnev().equals("Sándor"))
-        );
-        people_list.setItems(content);
+        current_content = query_students_by_filter((Student student) -> student.getKnev().equals("Sándor"));
+        people_list.setItems(FXCollections.observableArrayList(current_content));
     }
 
     @FXML
     protected void onKecskemetButtonClick() {
-        ObservableList<String> content = FXCollections.observableArrayList(
-                query_students_by_filter((Student student) -> student.getPlaceOfLiving().equals("Kecskemét"))
-        );
-        people_list.setItems(content);
+        current_content = query_students_by_filter((Student student) -> student.getPlaceOfLiving().equals("Kecskemét"));
+        people_list.setItems(FXCollections.observableArrayList(current_content));
     }
 
     @FXML
     protected void onFrom1996Click() {
-        ObservableList<String> content = FXCollections.observableArrayList(
-                query_students_by_filter((Student student) -> student.getBirthdate().startsWith("1996"))
-        );
-        people_list.setItems(content);
+        current_content = query_students_by_filter((Student student) -> student.getBirthdate().startsWith("1996"));
+        people_list.setItems(FXCollections.observableArrayList(current_content));
     }
 
     @FXML
     protected void onPartOf10AButtonClick() {
-        ObservableList<String> content = FXCollections.observableArrayList(
-                query_students_by_filter((Student student) -> student.getSchoolclass().equals("10/A"))
-        );
-        people_list.setItems(content);
+        current_content = query_students_by_filter((Student student) -> student.getSchoolclass().equals("10/A"));
+        people_list.setItems(FXCollections.observableArrayList(current_content));
     }
 
     private String[] query_students_by_filter(StudentFilter filter) {
         Student[] matches = new Student[] {};
+        current_content = new String[] {};
 
         for (Student student : students.getStudents()) {
             if (filter.is_matched(student)) {
