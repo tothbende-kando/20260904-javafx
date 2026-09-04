984fead9461b8909711fc85257fae449c7d87030
Weicat <weicat@mailfence.com>
2026-09-04 12:30:30 +0200

    Change icon

--- src/main/java/com/example/diakok/DiakApplication.java
+++ src/main/java/com/example/diakok/DiakApplication.java
@@ -3,6 +3,7 @@ package com.example.diakok;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,7 @@ public class DiakApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 960, 540);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.getIcons().add(new Image("file:diak.png"));
         stage.show();
     }
 
