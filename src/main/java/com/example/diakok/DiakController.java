baec5ecfdc3113831653fae20b95d24c10f1690c
Weicat <weicat@mailfence.com>
2026-09-04 11:50:57 +0200

    Renamed things

--- src/main/java/com/example/diakok/DiakController.java
+++ src/main/java/com/example/diakok/DiakController.java
@@ -3,7 +3,7 @@ package com.example.diakok;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class DiakController {
     @FXML
     private Label welcomeText;
 
